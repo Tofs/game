c062a3811eeb540a69e48e1553bb458a8188ea9c
Anton <anton.einarson@gmail.com>
2023-11-05 16:23:32 +0100

    added changes to main.py

--- main.py
+++ main.py
@@ -2,40 +2,27 @@ from cmath import log
 import pygame
 import logging
 import time
+import drawSurface
 
-class GameObject:
-    def __init__(self):
-        self.colour = "pink"
-        self.position = pygame.Vector2(0,0)
-        self.velocity = pygame.Vector2(0,0)
-
-    def update(self):
-        self.position += self.velocity
-        self.velocity *= 0.9
 
 #configure logging
 logging.basicConfig(level=logging.DEBUG)
-
 logging.info("Start game!")
 
-logging.debug("Configure pygame")
-pygame.init()
-
-# TODO: Read display settings from settings file.
-screen = pygame.display.set_mode((1280, 800))
-clock = pygame.time.Clock()
 
 #object deff
 objectList = []
-activeObject = GameObject()
+activeObject = drawSurface.GameObject()
 objectList.append(activeObject)
 
+# create surface
+logging.debug("Configure pygame")
+game = drawSurface.drawSurface(objectList)
+clock = pygame.time.Clock()
 
 logging.info("Enter mainloop")
 run = True
 
-def DrawObject(drawObject: GameObject):
-    pygame.draw.circle(screen, drawObject.colour, drawObject.position, 40)
 
 
 while run:
@@ -45,7 +32,10 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                activeObject = drawSurface.GameObject()
+                objectList.append(activeObject)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -56,9 +46,9 @@ while run:
         activeObject.velocity.x -= 30 * dt
     if keys[pygame.K_d]:
         activeObject.velocity.x += 30 * dt
-    if keys[pygame.K_SPACE]:
-        activeObject = GameObject()
-        objectList.append(activeObject)
+
+
+
 
 
     logging.info("Update")
@@ -66,14 +56,7 @@ while run:
         updateObject.update()
 
 
-
-    logging.info("Draw")
-    screen.fill("blue")
-
-    for drawObject in objectList:
-        DrawObject(drawObject)
-
-    pygame.display.flip()
+    game.draw()
 
     execTime = time.time() - start_time
     logging.debug("loop execution time: " + str(execTime))
